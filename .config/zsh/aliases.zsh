052331f0c26d527d78ad6395508f7820b973d144
Kat <10361870+katattakd@users.noreply.github.com>
2020-04-06 08:22:06 -0700

    Setup imv and mpv to use wayland

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,11 +10,11 @@ alias egrep='egrep --color=auto'
 
 alias lsblk='lsblk -o NAME,SIZE,TYPE,FSTYPE,MOUNTPOINT,LABEL'
 
-alias arp='sudo arp-scan -v -i 5'
-
 alias rm='rm -ir'
 alias cp='cp -ir'
 alias mv='mv -i'
 alias mkdir='mkdir -p'
 alias chown='chown -hR'
 alias ln='ln -is'
+
+alias imv='imv-wayland'
