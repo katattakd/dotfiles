67f04b670b743177680252d0cd2107aa9ba878db
katattakd <10361870+katattakd@users.noreply.github.com>
2020-05-02 04:51:06 -0700

    Added pulseaudio

--- setup-alpine.sh
+++ setup-alpine.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo adduser katattakd input
 sudo adduser katattakd video
+sudo adduser katattakd audio
 sudo apk add sway xkeyboard-config
 
-sudo apk add swaylock swayidle termite j4-dmenu-desktop
+sudo apk add swaylock termite j4-dmenu-desktop
 
 sudo apk add zsh zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search fzf fzf-zsh-completion
 
@@ -20,4 +21,12 @@ sudo rc-update add ufw
 sudo apk add iwd
 sudo rc-update del wpa_supplicant
 sudo rc-update add iwd
+
+sudo apk add firefox imv mpv
+sudo apk add ffmpeg imagemaigck
+sudo apk add pulseaudio pulseaudio-alsa alsa-plugins-pulse
+sudo apk add pulsemixer alsa-utils
+
+sudo apk add coreutils util-linux
+
 echo "Please reboot for changes to take effect."
