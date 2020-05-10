ed6a1da9de46ccaff3685d31fd5f77965f362097
Kat <10361870+katattakd@users.noreply.github.com>
2020-05-09 21:42:34 -0700

    Minor fixes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ln -s ~/dotfiles/.config/* ~/.config
 ln -s ~/dotfiles/.ssh/* ~/.ssh
-ln -s ~/dotfiles/.z* ~
+ln -s ~/dotfiles/.bash* ~
 sudo ln -s $HOME/dotfiles/.mozilla/autoconfig.js /usr/lib/firefox/defaults/pref/autoconfig.js
 sudo ln -s $HOME/dotfiles/.mozilla/firefox.cfg /usr/lib/firefox/firefox.cfg
