03e8228e4edb8dbf939bac0bf9dc4b6782791bb8
Kat <10361870+katattakd@users.noreply.github.com>
2020-05-10 15:51:06 -0700

    Home directory tweaks

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-mkdir -p ~/.config ~/.ssh
+mkdir -p ~/.config ~/.ssh ~/Downloads ~/Desktop
+
+ln -s /dev/null ~/.ash_history
 ln -s ~/dotfiles/.config/* ~/.config
 ln -s ~/dotfiles/.ssh/* ~/.ssh
 ln -s ~/dotfiles/.mozilla/user.js $(ls -d ~/.mozilla/firefox/*/)user.js
+
+ln -s ~/Downloads ~/Desktop/
+ln -s ~/.config ~/Desktop/
+ln -s ~/.mozilla/firefox ~/.config/firefox
+ln -s ~/dotfiles ~/Desktop/
