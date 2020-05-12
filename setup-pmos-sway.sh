19d9b8744513312b5f043d7f8ad64f94276c263b
Kat <10361870+katattakd@users.noreply.github.com>
2020-05-12 11:16:17 -0700

    Edited packages and sway config

--- setup-pmos-sway.sh
+++ setup-pmos-sway.sh
@@ -6,28 +6,28 @@
 # Install additional packages
 alias addpkg="nix-env -f '<nixpkgs>' -iA"
 nix-env -e inetutils binutils-wrapper
-addpkg i3status termite grim slurp mpv firefox-wayland			# GUI things
-addpkg bash coreutils-full unixtools debianutils ncurses openssh procps	# CLI essentials
+
+addpkg bash coreutils-full unixtools debianutils man ncurses expect	# Coreutils
 	nix-env --set-flag priority 11 hostname
-	addpkg sysstat expect mosh xdg_utils powertop
-addpkg htop fish tree less mandoc neovim neofetch			# CLI tweaks
-addpkg git diffutils gnupatch						# Git tools
 addpkg dos2unix gawk bc gnused gnugrep ed				# Text tools
-addpkg file patchutils findutils libcap sharutils			# File tools
-addpkg zstd lz4 lzop gzip brotli bzip2 lzma gnutar cpio unzip		# Archive tools
-addpkg parted dosfstools e2fsprogs f2fs-tools cryptsetup hdparm		# Filesystem tools
-addpkg kmod pciutils usbutils sysfsutils utillinux kbd lsof		# Kernel tools
-addpkg microcom setserial						# Serial tools
+addpkg git diffutils openssh						# Git tools
+addpkg file gnupatch patchutils findutils libcap sharutils		# File tools
 addpkg ffmpeg-full imagemagickBig sox youtube-dl exiftool		# Multimedia tools
-addpkg apulse alsaUtils beep						# Audio
+addpkg zstd lz4 lzop gzip brotli bzip2 lzma gnutar cpio unzip		# Archive tools
 addpkg gcc automake cmake gnumake rustup go				# Dev tools
 	nix-env --set-flag priority 9 gcc-wrapper
 	addpkg binutils
+addpkg kmod pciutils usbutils sysfsutils utillinux kbd lsof		# Kernel tools
+addpkg htop procps sysstat powertop					# Monitoring tools
 addpkg wget curlFull iproute dnsutils nmap netcat-gnu iptables iputils	# Networking tools
 	nix-env --set-flag priority 9 nettools
 	addpkg inetutils bridge-utils ipcalc
+addpkg microcom setserial						# Serial tools
+addpkg apulse alsaUtils beep						# Audio
+addpkg parted dosfstools e2fsprogs f2fs-tools cryptsetup hdparm		# Filesystem tools
 
-# TODO: Replace Busybox with full-featured commands.
+addpkg fish tree less neovim neofetch					# CLI tweaks
+addpkg i3status termite grim slurp mpv firefox-wayland			# GUI things
 
 nix-collect-garbage -d
 
