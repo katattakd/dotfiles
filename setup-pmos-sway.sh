fac5b7c8e7940957d2b4a37446c36c434792799d
Kat <10361870+katattakd@users.noreply.github.com>
2020-06-10 03:30:33 -0700

    Removed unused dependencies

--- setup-pmos-sway.sh
+++ setup-pmos-sway.sh
@@ -25,11 +25,11 @@ nix-env -u
 sudo apk upgrade -a
 
 # Coreutils
-sudo apk add alpine-sdk pmboostrap postmarketos-base
-sudo apk add docs usbutils cmd:less cmd:tree
+sudo apk add alpine-sdk docs postmarketos-base
+sudo apk add cmd:less cmd:pmbootstrap cmd:tree cmd:usb-devices
 
 # Text tools
-sudo apk add cmd:awk cmd:brotli cmd:diff cmd:grep cmd:sed
+sudo apk add cmd:brotli cmd:diff cmd:grep
 
 # Multimedia tools
 sudo apk add cmd:exiftool cmd:ffmpeg cmd:magick cmd:sox cmd:youtube-dl
@@ -41,8 +41,7 @@ nix-env -f '<nixpkgs>' -iA rustup
 sudo apk add cmd:htop cmd:powertop
 
 # Networking tools
-sudo apk add iproute2 ipcalc
-sudo apk add cmd:dig cmd:tshark cmd:ufw ufw-openrc
+sudo apk add cmd:dig cmd:ip cmd:tshark cmd:ufw ufw-openrc
 sudo apk add cmd:nmap cmd:ncat cmd:nping nmap-scripts
 
 # Audio tools
@@ -50,7 +49,7 @@ sudo apk add cmd:alsamixer
 nix-env -f '<nixpkgs>' -iA apulse
 
 # ClI tweaks
-sudo apk add cmd:neofetch cmd:nvim fish-tools
+sudo apk add cmd:fish cmd:neofetch cmd:nvim
 
 # GUI things
 sudo apk add cmd:i3status cmd:termite cmd:grim cmd:slurp cmd:mpv cmd:imv firefox
@@ -67,11 +66,5 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 sudo rc-update del sshd
 sudo ufw enable
 sudo rc-update add ufw
-
-# Wireless improvements
-sudo apk add iwd
-printf "[device]\nwifi.backend=iwd\n" | sudo tee /etc/NetworkManager/conf.d/01-iwd.conf
-sudo rc-update del wpa_supplicant
-sudo rc-update add iwd
 
 echo "Please reboot for changes to take effect."
