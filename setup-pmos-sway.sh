b3a3cbcac28c06f1e6f576254bec1ef3e2241dcc
Kat <10361870+katattakd@users.noreply.github.com>
2020-06-10 01:19:57 -0700

    Various changes

--- setup-pmos-sway.sh
+++ setup-pmos-sway.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # TODO: Switch back to Alacritty once it gets GLES2 support.
-# TODO: Add imv once it works on Nix aarch64.
 
 # ----- User customizations ------
 
@@ -8,35 +7,58 @@
 sh setup.sh
 set -euo pipefail
 
-# Install additional packages
-alias addpkg="nix-env -f '<nixpkgs>' -iA"
-nix-env -e inetutils binutils-wrapper
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-nix-channel --add https://nixos.org/channels/nixos-20.03
-nix-channel --update
+# Install Nix
+sudo apk add curl
+sudo mkdir /nix
+sudo chown katattakd /nix
+echo "#!/bin/sh" > ~/.profile
 
-addpkg coreutils-full unixtools debianutils man man-pages expect	# Coreutils
-	nix-env --set-flag priority 11 hostname
-addpkg bash ncurses dialog readline less				# Console tools
-addpkg dos2unix gawk bc gnused gnugrep ed				# Text tools
-addpkg git diffutils openssh						# Git tools
-addpkg file gnupatch patchutils findutils libcap sharutils		# File tools
-addpkg ffmpeg-full imagemagickBig sox youtube-dl exiftool		# Multimedia tools
-addpkg zstd lz4 lzop gzip brotli bzip2 lzma gnutar cpio unzip		# Archive tools
-addpkg gcc automake cmake gnumake rustup go				# Dev tools
-	nix-env --set-flag priority 9 gcc-wrapper
-	addpkg binutils
-addpkg kmod pciutils usbutils sysfsutils utillinux kbd lsof		# Kernel tools
-addpkg htop procps sysstat powertop					# Monitoring tools
-addpkg wget curlFull iproute dnsutils nmap netcat-gnu iptables iputils	# Networking tools
-	nix-env --set-flag priority 9 nettools
-	addpkg inetutils bridge-utils ipcalc wireshark-cli
-addpkg microcom setserial						# Serial tools
-addpkg apulse alsaUtils beep						# Audio
-addpkg parted dosfstools e2fsprogs f2fs-tools cryptsetup hdparm		# Filesystem tools
-addpkg fish tree neovim neofetch					# CLI tweaks
-addpkg i3status termite grim slurp mpv					# GUI things
-	nix-env -f '<nixos-20.03>' -iA firefox
+curl https://nixos.org/nix/install -o /tmp/install.sh
+sh /tmp/install.sh --no-daemon
+
+source ~/.profile
+sudo apk del curl
+
+# Update installed packages
+nix-channel --update
+nix-env -u
+sudo apk upgrade -a
+
+# Coreutils
+sudo apk add alpine-sdk docs pmbootstrap usbutils cmd:find cmd:less cmd:tree
+
+# Text tools
+sudo apk add cmd:awk cmd:diff cmd:grep cmd:sed
+
+# Multimedia tools
+sudo apk add cmd:exiftool cmd:ffmpeg cmd:magick cmd:sox cmd:youtube-dl
+
+# Archive tools
+sudo apk add cmd:brotli cmd:bzip2 cmd:cpio cmd:gzip cmd:lz4 cmd:unzip cmd:xz cmd:zstd
+
+# Dev tools
+nix-env -f '<nixpkgs>' -iA automake cmake rustup go
+
+# Monitoring tools
+sudo apk add cmd:htop cmd:powertop
+
+# Networking tools
+sudo apk add net-tools iputils iproute2 bridge-utils ipcalc
+sudo apk add cmd:dig cmd:ssh cmd:tshark cmd:ufw cmd:whois ufw-openrc
+sudo apk add cmd:nmap cmd:ncat cmd:nping nmap-scripts
+
+# Audio tools
+sudo apk add cmd:alsamixer
+nix-env -f '<nixpkgs>' -iA apulse
+
+# Filesystem tools
+sudo apk add cmd:cryptsetup cmd:parted dosfstools e2fsprogs f2fs-tools
+
+# ClI tweaks
+sudo apk add cmd:neofetch cmd:nvim fish-tools
+
+# GUI things
+sudo apk add cmd:i3status cmd:termite cmd:grim cmd:slurp cmd:mpv cmd:imv firefox
 
 nix-store --optimise
 nix-collect-garbage -d
@@ -48,9 +70,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 
 # Security improvements
 sudo rc-update del sshd
-
-# Additional security improvements
-sudo apk add ufw ufw-openrc
 sudo ufw enable
 sudo rc-update add ufw
 
