5d0f40468aa2e736a338d548c4e318ba317d4bfe
Kat <10361870+katattakd@users.noreply.github.com>
2020-06-11 05:48:53 +0000

    Tweaked dependencies

--- setup-pmos-sway.sh
+++ setup-pmos-sway.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 # TODO: Switch back to Alacritty once it gets GLES2 support.
-# TODO: Setup rustup on aarch64 without requiring Nix.
 
 # ----- User customizations ------
 
 # Create config symlinks
 sh setup.sh
 set -euo pipefail
+
+# Setup Nix
+touch ~/.profile
+
+curl https://nixos.org/nix/install -o /tmp/install.sh
+sh /tmp/install.sh --no-daemon
+
+source ~/.profile
 
 # Update installed packages
 sudo apk -U upgrade -a
@@ -29,7 +36,14 @@ sudo apk add cmd:dig cmd:ip cmd:tshark cmd:ufw ufw-openrc
 sudo apk add cmd:nmap cmd:ncat cmd:nping nmap-scripts
 
 # Audio tools
-sudo apk add cmd:alsamixer cmd:pactl cmd:pulseaudio cmd:pulsemixer
+sudo apk add cmd:alsamixer cmd:pactl cmd:pulsemixer pulseaudio
+
+# RTL-SDR tools
+sudo apk add cmd:rtl_sdr
+nix-env -f '<nixpkgs>' -iA gqrx
+
+# Dev tools
+nix-env -f '<nixpkgs>' -iA rustup
 
 # ClI tweaks
 sudo apk add cmd:fish cmd:neofetch cmd:nvim
@@ -37,14 +51,9 @@ sudo apk add cmd:fish cmd:neofetch cmd:nvim
 # GUI things
 sudo apk add cmd:i3status cmd:termite cmd:grim cmd:slurp cmd:mpv cmd:imv firefox
 
-# Setup rustup
-touch ~/.profile
-
-curl https://nixos.org/nix/install -o /tmp/install.sh
-sh /tmp/install.sh --no-daemon
-
-source ~/.profile
-nix-env -f '<nixpkgs>' -iA rustup
+# Finishing touches
+sudo rm -r /etc/apk/cache
+sudo rm /var/cache/apk/*.apk
 nix-store --optimise
 nix-collect-garbage -d
 
