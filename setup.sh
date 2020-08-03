43ca9b59d2767ef27971e9673efe26118f176867
Kat <10361870+katattakd@users.noreply.github.com>
2020-08-03 02:53:15 -0700

    Fixed Firefox setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ ln -sf /dev/null ~/.ash_history
 ln -sf /dev/null ~/.bash_history
 ln -s ~/dotfiles/.config/* ~/.config
 ln -s ~/dotfiles/.ssh/* ~/.ssh
-echo ~/.mozilla/firefox/*.default | xargs -n 1 ln -s ~/dotfiles/.mozilla/user.js
+echo ~/.mozilla/firefox/*.default* | xargs -n 1 ln -s ~/dotfiles/.mozilla/user.js
 
 ln -sf ~/.bashrc ~/.bash_profile
 rm ~/.bash_logout
