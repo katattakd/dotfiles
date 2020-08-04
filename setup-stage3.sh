42c419e620e632dc495747ef378b930db7f9e99a
Kat <10361870+katattakd@users.noreply.github.com>
2020-08-03 20:26:07 -0700

    Added font packages

--- setup-stage3.sh
+++ setup-stage3.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Sway + essential packages
-sudo pacman -S --asexplicit --noconfirm sway swaylock i3status grimshot termite wl-clipboard xorg-server-xwayland
+sudo pacman -S --asexplicit --noconfirm sway swaylock i3status grimshot noto-fonts termite wl-clipboard xorg-server-xwayland
 
 # CLI customizations
 sudo pacman -S --asexplicit --noconfirm exa fish neovim
@@ -13,4 +13,4 @@ sudo pacman -S --asexplicit --noconfirm pulseaudio-alsa pulsemixer
 
 # GUI essentials
 sudo pacman -S --asexplicit --noconfirm imv mpv termite
-sudo pacman -S --asexplicit --noconfirm firefox hunspell-en_US
+sudo pacman -S --asexplicit --noconfirm firefox noto-fonts-cjk noto-fonts-emoji hunspell-en_US
