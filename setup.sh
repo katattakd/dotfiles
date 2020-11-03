96e74008a43ee49a2da2b5984d1aaaf3b2ce7fdf
Kat <10361870+katattakd@users.noreply.github.com>
2020-11-02 20:17:56 -0800

    Tweaked mpv, improved curl

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ mkdir -p ~/.config ~/.ssh ~/Downloads ~/Desktop
 ln -sf /dev/null ~/.ash_history
 ln -sf /dev/null ~/.bash_history
 ln -s ~/dotfiles/.config/* ~/.config
+ln -s ~/dotfiles/.curlrc ~/.curlrc
 ln -s ~/dotfiles/.ssh/* ~/.ssh
 echo ~/.mozilla/firefox/*.default* | xargs -n 1 ln -s ~/dotfiles/.mozilla/user.js
 
