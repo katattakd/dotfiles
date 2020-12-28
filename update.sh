378264178b6c3e24413cf602b819966cd109d08c
Kat <10361870+katattakd@users.noreply.github.com>
2020-12-28 11:17:52 -0800

    Various tweaks

--- update.sh
+++ update.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+# Maintance task: Remove unused cache files
+find ~/.cache/ -atime +28 -print -delete
+
+# Maintance task: Remove old logs
+sudo journalctl --vacuum-time=2weeks
+
 # Update installed packages
 pamac upgrade -a --devel
+
+# Update config files
+sudo -E DIFFPROG='nvim -d' pacdiff
 
 # Remove orphaned packages
 pamac remove -o
 
 # Clean cache
-pamac clean -b
+pamac clean -k 3
+pamac clean --build-files -k 0
+pamac clean --uninstalled -k 0
 
 # Update Fish completions
 fish -c fish_update_completions
