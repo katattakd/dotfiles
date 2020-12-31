aaec8b7bf34f64dd45a882dcf4093fa57cf6a3fe
Kat <10361870+katattakd@users.noreply.github.com>
2020-12-30 18:15:27 -0800

    Update setup-manjaro-minimal.sh

--- setup-manjaro-minimal.sh
+++ setup-manjaro-minimal.sh
@@ -4,14 +4,14 @@
 # - https://github.com/alacritty/alacritty/issues/128
 # TODO: Get Anbox working.
 
-# Create config symlinks
-sh setup.sh
-
 # Install updates and flag all non-core packages for removal
 sh setup-stage1.sh
 
 # Install and setup essential tools
 sh setup-stage2.sh
+
+# Create config symlinks
+sh setup.sh
 
 # Install and setup a graphical enviornment
 sh setup-stage3.sh
@@ -21,8 +21,5 @@ sh setup-stage4.sh
 
 # Install optional GUI tools, remove unused packages
 sh setup-stage5.sh
-
-# Disable SSH
-sudo systemctl disable sshd
 
 echo "Installation finished. Rebooting is recommended."
