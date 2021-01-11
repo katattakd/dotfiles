4fc321e88a5a697c1f1617ee18dd53200be588fa
Kat <10361870+katattakd@users.noreply.github.com>
2021-01-11 11:16:42 -0800

    Tweaked fish color scheme

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -12,17 +12,18 @@ function fish_prompt
 
 	set_color $fish_color_user
 	printf "$USER"
-	set_color normal
+	set_color $fish_color_comment
 	printf ' at '
 
 	set_color $fish_color_host
 	echo -n (prompt_hostname)
-	set_color normal
+	set_color $fish_color_comment
 	printf ' in '
 
 	set_color $fish_color_cwd
 	echo (prompt_pwd)
 
-	set_color normal
+	set_color $fish_color_comment
 	printf '↪ '
+	set_color normal
 end
