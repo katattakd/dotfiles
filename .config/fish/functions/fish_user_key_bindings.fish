66098b63ce668452c9bbe30de17bbda4f4fafdcd
Kat <10361870+katattakd@users.noreply.github.com>
2021-03-26 10:29:22 -0700

    Tweaked fish configuration

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,22 @@
 function fish_user_key_bindings
 	bind \cH backward-kill-path-component
+	bind -k f1 "run_command 'exa --all --classify --grid --group-directories-first'"
+	bind -k f2 "run_command 'exa --all --tree --level=4 --long --git --header --extended --git-ignore'"
+	bind -k f3 "run_command lazygit"
+
+	bind -k f9 "change_dir .."
+	bind -k f10 "change_dir ~/Desktop"
+	bind -k f11 "change_dir ~/dotfiles"
+	bind -k f12 "change_dir ~"
 end
+
+function change_dir
+	cd $argv
+	commandline -f repaint
+end
+
+function run_command
+	commandline -r "$argv"
+	commandline -f execute
+end
+
