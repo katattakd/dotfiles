0cfca03f6f576430d058b16a663bde3dfa47cb5b
Kat <10361870+katattakd@users.noreply.github.com>
2021-03-31 10:44:31 -0700

    Setup systemd-resolved

--- configure_system.sh
+++ configure_system.sh
@@ -3,6 +3,7 @@
 # Configure networking
 sudo systemctl enable NetworkManager
 sudo systemctl disable systemd-networkd
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
 # Disable SSH (Manjaro [ARM] specific)
 sudo systemctl disable sshd
