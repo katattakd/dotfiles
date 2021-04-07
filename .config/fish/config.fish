d7ef5793fa43d021be27702e8ecfe6d16de0ee4d
Kat <10361870+katattakd@users.noreply.github.com>
2021-04-07 11:57:07 -0700

    Minor tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,18 @@
 set -gx CARGO_HOME $HOME/.local/share/cargo
 set -gx RUSTUP_HOME $HOME/.local/share/rustup
+
+function run
+	$argv &>/dev/null &; disown
+end
+
+function play
+	run mpv $argv
+end
+
+function view
+	run mpv --profile=image $argv
+end
+
+function flatrun
+	run flatpak run --user $argv
+end
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,7 +3,7 @@ function fish_user_key_bindings
 	bind -k f1 "run_command 'exa --all --classify --grid --group-directories-first'"
 	bind -k f2 "run_command 'exa --all --tree --level=4 --long --git --header --extended --git-ignore'"
 	bind -k f3 "run_command lazygit"
-	bind -k f4 "commandline -i ' &>/dev/null &'"
+	bind -k f4 "commandline -C 0 && commandline -i 'run ' && commandline -f execute"
 	bind -k f5 "run_command reset"
 
 	bind -k f9 "change_dir .."
