d9b19c25ca0c4374a02bc21ebd79387976aa31b5
Kat <10361870+katattakd@users.noreply.github.com>
2021-04-13 02:25:41 -0700

    Tweaked mpv configuration, bug fixes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,6 +13,14 @@ function view
 	run mpv --profile=image $argv
 end
 
-function flatrun
-	run flatpak run --user $argv
+if command -v flatpak > /dev/null
+	function flatrun
+		run flatpak run --user $argv
+	end
+
+	abbr -ga flatseal 'flatrun com.github.tchx84.Flatseal'
+	abbr -ga gimp 'flatrun org.gimp.GIMP'
+	abbr -ga libreoffice 'flatrun org.libreoffice.LibreOffice'
+	abbr -ga rawtherapee 'flatrun com.rawtherapee.RawTherapee'
 end
+
