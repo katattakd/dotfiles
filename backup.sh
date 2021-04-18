6b59c200686df5c6580969dccdee4f324b5ec302
Kat <10361870+katattakd@users.noreply.github.com>
2021-04-17 19:45:31 -0700

    Tweaked backup script

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -am --delete -v ~ ~/.local/share/fish/fish_history --exclude ".cache" --exclude ".config" --exclude ".local" --exclude ".mozilla" --exclude ".ssh" --exclude ".var" --exclude "dotfiles" --exclude ".identity" --exclude "packages.txt" $@
+rsync -am --delete --delete-excluded -v ~ ~/.local/share/fish/fish_history --exclude ".cache" --exclude ".config" --exclude ".local" --exclude ".mozilla" --exclude ".ssh" --exclude ".var" --exclude "dotfiles" --exclude ".identity" --exclude "packages.txt" $@
