0e7666272794af7a20e055c77cc45d31496edb07
Kat <10361870+katattakd@users.noreply.github.com>
2021-04-22 06:42:04 -0700

    Added termina copy/move/paste commands

--- .config/fish/conf.d/navigate.fish
+++ .config/fish/conf.d/navigate.fish
@@ -2,6 +2,31 @@ function navigate --on-variable PWD
 	eval $_fish_abbr_ls
 end
 
+function fc
+	set -l tocopy ""
+	for arg in $argv
+		set -a tocopy (echo "file://"(string escape --style=url (realpath -s "$arg")))
+		string escape (realpath -s "$arg")
+	end
+	echo $tocopy | wl-copy
+end
+
+function fp
+	for file in (string escape -n (string split -n ' ' (wl-paste)))
+		if test (string sub -l 8 $file) = 'file:///'
+			cp -iav (string unescape --style=url (string unescape (string sub -s 8 "$file"))) .
+		end
+	end
+end
+
+function fm
+	for file in (string escape -n (string split -n ' ' (wl-paste)))
+		if test (string sub -l 8 $file) = 'file:///'
+			mv -iv (string unescape --style=url (string unescape (string sub -s 8 "$file"))) .
+		end
+	end
+end
+
 function .
 	navigate
 end
