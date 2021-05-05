87a6068c485b9d7e370c34ad1004d828b638eb6c
Kat <10361870+katattakd@users.noreply.github.com>
2021-05-04 17:31:10 -0700

    Minor tweaks

--- configure_user.sh
+++ configure_user.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Create new user directories
+# Setup new XDG dirs
 mkdir -p ~/Projects
+cp ~/dotfiles/.config/user-dirs.dirs ~/.config/user-dirs.dirs
 
 # Create symlinks to config files
-cp ~/dotfiles/.config/user-dirs.dirs ~/.config/user-dirs.dirs
 rm -r ~/.config/fish
 ln -sf ~/dotfiles/.config/* ~/.config
 
