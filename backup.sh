5108ae84cd5f079852f5ec6d909bb62798385447
Kat <10361870+katattakd@users.noreply.github.com>
2021-05-08 11:39:49 -0700

    Tweaked backup script

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avmR --delete --delete-excluded ~/./ ~/./.local/share/fish/fish_history --exclude ".*/" --exclude "checkout" --exclude "dotfiles" --exclude ".identity" --exclude "packages.txt" $@
+rsync -avmR --delete --delete-excluded ~/./ ~/./.local/share/fish/fish_history --include ".gitconfig" --exclude ".*" --exclude "checkout" --exclude "packages.txt" $@
